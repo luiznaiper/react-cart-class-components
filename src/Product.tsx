90e46f07433441a5dfcdb8f60c1766f2c46d7a6a
luiznaiper <luiz_naiper@hotmail.com>
2023-01-13 06:28:47 -0300

    :construction: Class 44

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,10 +1,36 @@
 import { Component, ReactNode } from 'react';
 
+interface stylesInline {
+  product: {
+    border: string;
+    boxShadow: string;
+    width: string;
+    padding: string;
+    borderRadius: string;
+  };
+  img: {
+    width: string;
+  };
+}
+
+const styles: stylesInline = {
+  product: {
+    border: '1 px solid #eee',
+    boxShadow: '0 5px 5px rgba(0,0,0,0.1)',
+    width: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px',
+  },
+  img: {
+    width: '100%',
+  },
+};
+
 class Product extends Component {
   render(): ReactNode {
     const { product } = this.props;
     return (
-      <div>
+      <div style={styles.product}>
         <img alt={product.name} src={product.img} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,27 @@
 import { Component, ReactNode } from 'react';
 import Product from '../Product';
 
+interface stylesInline {
+  products: {
+    display: string;
+    flexDirection: string;
+    justifyContent: string;
+  };
+}
+
+const styles: stylesInline = {
+  products: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-beetween',
+  },
+};
+
 class Products extends Component {
   render(): ReactNode {
     const { products, addToCart } = this.props;
     return (
-      <div>
+      <div style={styles.products}>
         {products.map((product) => (
           <Product key={product.name} addToCart={addToCart} product={product} />
         ))}
